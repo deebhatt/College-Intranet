7a80139f277d2b7dda2414aed0d582f3f501f19e
Deepak <deepakbhatt1990@gmail.com>
2015-04-18 16:53:34 +0530

    Transfer Copy

---
+++ src/play/rest/serve/reminins.java
@@ -0,0 +1,18 @@
+package com.test.lko;
+
+class Mri{
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+}
+public class reminins {
+
+
+	public void Testingsum()
+	{
+		Mri mr = new Mri();
+		
+		
+	}
+}
